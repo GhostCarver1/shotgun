d4a05199c1652325fd09a3d97fd0ad9d4afab67f
GhostCarver1 <156866378+GhostCarver1@users.noreply.github.com>
2026-05-19 18:00:26 -0600

    Delete src directory

--- src/connection/routes.h
+++
@@ -1,8 +0,0 @@
-#ifndef ROUTES_H
-#define ROUTES_H
-
-#include "../game/game_state.h"
-
-void handle_request(int client_fd, const char *buffer, GameState *game_state);
-
-#endif
--- src/game/turn_logic.h
+++
@@ -1,6 +0,0 @@
-#ifndef TURN_LOGIC_H
-#define TURN_LOGIC_H
-
-void do_turn(GameState *game_state);
-
-#endif
