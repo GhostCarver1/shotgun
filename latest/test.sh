29da1f7646fcc3232d2e862b5e7c627b580d171c
Tyler Carver <tyler_carver@outlook.com>
2026-06-12 15:58:53 -0600

    made it so that the test.sh uses appropriate database instead of the main one

--- latest/test.sh
+++ latest/test.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
+set -e
 
 echo "INITIALIZING DATABASE"
 
-sudo -u postgres psql \
-    -d shotgun \
-    -f database/init.sql \
-    > /dev/null
+set -a
+source env.test
+set +a
 
-if [ $? -ne 0 ]; then
-    echo "DATABASE INIT FAILED"
-    exit 1
-fi
+sudo service postgresql start
+
+sudo -u postgres psql -c "CREATE ROLE shotgun_admin LOGIN PASSWORD 'password';" || true
+
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS shotgun_test;"
+sudo -u postgres createdb shotgun_test -O shotgun_admin
+
+sudo -u postgres psql -d shotgun_test -f database/init.sql > /dev/null
+sudo -u postgres psql -d shotgun_test -f database/test_init.sql > /dev/null
 
 rm -f shotgun
 make clean || true
 make -f makefile.mk
-
-
-if [ $? -ne 0 ]; then
-    echo "COMPILE FAILED"
-    exit 1
-fi
 
 echo "PROGRAM COMPILED"
 echo "RUNNING THE SERVER"
@@ -30,9 +29,9 @@ SERVER_PID=$!
 
 echo "Server PID: $SERVER_PID"
 
+sleep 2
+
 newman run postman/shotgun.postman_collection.json --env-var "host=localhost"
 
 echo "KILLING SERVER"
 kill $SERVER_PID
-
-
